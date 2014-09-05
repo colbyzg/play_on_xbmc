ad75fa7e6d8b20fd8b9b007d7384a9d7f0c61c00
colbyzg <colby.glass@gmail.com>
2014-09-05 05:18:34 -0400

    commit

--- README.md
+++ README.md
@@ -6,12 +6,14 @@ Short script to send YouTube videos to an XBMC instance.
 
 # Run without video ID:
 <pre>
-./play_on_xbmc.py
+# ./play_on_xbmc.py
 What is the ID of the YouTube video?
 > dQw4w9WgXcQ
+Playing 'Rick Astley - Never Gonna Give You Up'
 </pre>
 
 # Specify video ID:
 <pre>
 # ./play_on_xbmc.py dQw4w9WgXcQ
+Playing 'Rick Astley - Never Gonna Give You Up'
 </pre>
--- play_on_xbmc.py
+++ play_on_xbmc.py
@@ -13,8 +13,17 @@ def send_to_xbmc(v_id):
 	payload = {"jsonrpc": "2.0", "method": "Player.Open", "params":{"item": {"file" : "plugin://plugin.video.youtube/?action=play_video&videoid={}".format(v_id) }}, "id" : "1"}
 	requests.post(xbmc_url, auth=(xbmc_username, xbmc_password), data=json.dumps(payload))
 
+def get_title(v_id):
+	r = requests.get("http://gdata.youtube.com/feeds/api/videos/{}?v=2&alt=jsonc".format(v_id))
+	return json.loads(r.content)
+
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		send_to_xbmc(sys.argv[1])
+		j = get_title(sys.argv[1])
+		print("Playing '{}'".format(j["data"]["title"]))
 	else:
-		send_to_xbmc(raw_input("What is the ID of the YouTube video?\n> "))
+		v_id = raw_input("What is the ID of the YouTube video?\n> ")
+		send_to_xbmc(v_id)
+		j = get_title(v_id)
+		print("Playing '{}'".format(j["data"]["title"]))
