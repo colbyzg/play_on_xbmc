9573043a8957688230cfd534959412aba35253fe
colbyzg <colby.glass@gmail.com>
2014-09-06 01:56:28 -0400

    commit

--- README.md
+++ README.md
@@ -2,18 +2,18 @@ play_on_xbmc
 ======
 
 # Description:
-Short script to send YouTube videos to an XBMC instance.
+Short script to send YouTube videos to an XBMC instance. I can take a YouTube URL or video ID.
 
-# Without video ID:
+# Without URL or video ID:
 <pre>
-# ./play_on_xbmc.py
-What is the ID of the YouTube video?
+# ./play_on_xbmc.py 
+What is the URL or ID of the YouTube video?
 > dQw4w9WgXcQ
 Playing 'Rick Astley - Never Gonna Give You Up'
 </pre>
 
-# With video ID:
+# With URL:
 <pre>
-# ./play_on_xbmc.py dQw4w9WgXcQ
+# ./play_on_xbmc.py https://www.youtube.com/watch?v=dQw4w9WgXcQ
 Playing 'Rick Astley - Never Gonna Give You Up'
 </pre>
--- play_on_xbmc.py
+++ play_on_xbmc.py
@@ -3,10 +3,21 @@
 import requests
 import json
 import sys
+import urlparse
 
 xbmc_ip = "192.168.25.97"
 xbmc_username = "xbmc"
 xbmc_password = "xbmc"
+
+def main(v_id):
+	send_to_xbmc(v_id)
+	j = get_title(v_id)
+	print("Playing '{}'".format(j["data"]["title"]))
+
+def parse_id(url):
+	parsed_url = urlparse.urlparse(url)
+	query = urlparse.parse_qs(parsed_url.query)
+	return query["v"][0]
 
 def send_to_xbmc(v_id):
 	xbmc_url = "http://{}/jsonrpc?request=".format(xbmc_ip)
@@ -19,11 +30,15 @@ def get_title(v_id):
 
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
-		send_to_xbmc(sys.argv[1])
-		j = get_title(sys.argv[1])
-		print("Playing '{}'".format(j["data"]["title"]))
+		if "youtube" in sys.argv[1].lower():
+			v_id = parse_id(sys.argv[1])
+			main(v_id)
+		else:
+			main(sys.argv[1])
 	else:
-		v_id = raw_input("What is the ID of the YouTube video?\n> ")
-		send_to_xbmc(v_id)
-		j = get_title(v_id)
-		print("Playing '{}'".format(j["data"]["title"]))
+		respone = raw_input("What is the URL or ID of the YouTube video?\n> ")
+		if "youtube" in respone.lower():
+			v_id = parse_id(respone)
+			main(v_id)
+		else:
+			main(v_id)
